4a6b11a50f514d0cc7ffc2ebd806a54430f1e54e
Peter Shkenev <peter.shhh@yandex.ru>
2023-10-02 12:11:06 +0000

    LogSystem: use LogBackend
    Add methods to register backends and to delegate logging to them

--- Environment/log_system.cpp
+++ Environment/log_system.cpp
@@ -1,18 +1,26 @@
 #include "log_system.h"
+#include "log_backend.h"
 
 LogSystem::LogSystem(QObject* parent) : QObject(parent) {
 }
 
 LogSystem::~LogSystem() {}
 
+void LogSystem::addBackend(LogBackend *backend)
+{
+  backends.append(backend);
+}
+
 void LogSystem::clear() {
-  emit requestClearDisplayLog();
+  for (int i = 0; i < backends.size(); i++)
+    backends[i]->clear();
 }
 
 void LogSystem::generate(const QString& log) {
   QTime time = QDateTime::currentDateTime().time();
   QString LogData = time.toString("hh:mm:ss.zzz - ") + log;
-  emit requestDisplayLog(LogData);
+  for (int i = 0; i < backends.size(); i++)
+    backends[i]->writeLogLine(LogData);
 }
 
 /*
